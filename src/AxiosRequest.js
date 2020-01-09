df6782709a007fc113d2bb71226ef77fdc2df203
@wwq <2141826215@qq.com>
2020-01-09 14:24:48 +0800

    Axios相关知识

--- src/AxiosRequest.js
+++ src/AxiosRequest.js
@@ -1,1 +1,109 @@
-        
+import Axios from "axios";
+// 创建Axios实例
+// const instance = Axios.create({
+//     baseURL:"https://",
+//     timeout:5000
+// })
+
+// 调用实例发送Axios请求
+// instance({
+//     url:"/home"
+// }).then(res =>{
+//     console.log(res)
+// })
+
+// instance({
+//     url:"/home",
+//     params:{
+//         type:'pop',
+//         page:1
+//     } 
+// }).then(res =>{console.log(res)})
+
+
+// export function request(config) {
+//    const instance = Axios.create({
+//        baseURL:"设置默认路径",
+//        timeout:5000
+//    })
+// //    发送真正的网络请求
+//    instance(config).then(
+//        res => {
+//           console.log(res)
+//        }
+//    ).catch(err => {
+//        console.log(err)
+//    })
+// }
+// 通过回调方式将数据返回 uccess,failure传入的函数
+// export function request(config,success,failure) {
+//     const instance = Axios.create({
+//         baseURL:"",
+//         timeout:5000
+//     })
+//  //    发送真正的网络请求
+//     instance(config).then(
+//         res => {
+//            console.log(res);
+//            success(res)
+//         }
+//     ).catch(err => {
+//         console.log(err);
+//         failure(err)
+//     })
+//  }
+
+//  将数据和回调函数已对象的形式传入
+
+// export function request(config){
+//     const instance = Axios.create({
+//         baseURL:'',
+//         timeout:5000
+//     })
+//     instance(config).then((res)=>{
+//         config.success(res)
+//     }).catch((err)=>{
+//         config.filters(err)
+//     })
+// }
+
+// 结合Promise实现axios实例化
+// export function request(config){
+//     return new Promise((resolve,reject)=>{
+//         const instance = Axios.create({
+//             baseURL:'',
+//             timeout:5000
+//         })
+//         instance(config).then((res)=>{
+//             resolve(res)
+//         }).catch((err)=>{
+//             reject(err)
+//         })
+//     })
+// }
+
+
+export function request(config){
+        const instance = Axios.create({
+            baseURL:'',
+            timeout:5000
+        })
+       
+// Axios 拦截器   请求拦截
+       instance.interceptors.request.use(res=>{
+        //    res拦截参数是config,拦截后需要再将参数return 返回
+           console.log(res)
+           return res
+       },err =>{
+
+       })
+    //    响应拦截
+       instance.interceptors.response.use(res=>{
+        //   res为返回的结果，通常做数据的处理过滤等等
+        console.log(res)
+        return res
+       },err=>{
+
+       })
+       return instance(config)
+}
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import Axios from 'axios';
+// import Axios from 'axios';
 
 Vue.config.productionTip = false
 
@@ -18,17 +18,17 @@ new Vue({
 })
 
 // 默认get请求
-Axios({
-  url:'http://httpbin.org',
-  method:'get',  //设置请求方式
-  // 专门针对get请求的参数拼接   http://httpbin.org?type=pop&page=2  params = ?type=pop&page=2
-  params:{
-    type:"pop",
-    page:2
-  }
-}).then(data =>{
-  console.log(data)
-})
+// Axios({
+//   url:'http://httpbin.org',
+//   method:'get',  //设置请求方式
+//   // 专门针对get请求的参数拼接   http://httpbin.org?type=pop&page=2  params = ?type=pop&page=2
+//   params:{
+//     type:"pop",
+//     page:2
+//   }
+// }).then(data =>{
+//   console.log(data)
+// })
 // Axios请求方式二e
 // Axios.get();Axios.post()
 // Axios并发请求
@@ -44,17 +44,55 @@ Axios({
 //   console.log(result)
 // })
 
-Axios.all([
-  Axios({
-    url:'http://httpbin.org',
-  }),
-  Axios({
-    url:'http://httpbin.org',
-  })
-  // result =》 res1  res2 返回结果数组展开
-]).then(Axios.spread((res1,res2)=> { 
-  console.log(res1)
-  console.log(res2)
-}))
+// Axios.all([
+//   Axios({
+//     url:'http://httpbin.org',
+//   }),
+//   Axios({
+//     url:'http://httpbin.org',
+//   })
+//   // result =》 res1  res2 返回结果数组展开
+// ]).then(Axios.spread((res1,res2)=> { 
+//   console.log(res1)
+//   console.log(res2)
+// }))
+
+// 使用封装的Axios请求 
+import {request} from './AxiosRequest'
+// request({
+//   url:'http://httpbin.org',
+// },(res)=>{
+//   console.log(res)
+// },(err)=>{
+//   console.log(err)
+// })
+
+// 
+// const config = {
+// configdata:{
+//   url:'http://httpbin.org',
+// },
+// success:(res)=>{
+//    console.log(res)
+// },
+// filters:(err)=>{
+//   console.log(err)
+// }
+
+// }
+
+// request(config)
+
+// Promise实例Axios
+request({
+  url:'http://httpbin.org',
+}).then((res)=>{
+  console.log(res)
+}).catch((err)=>{
+  console.log(err)
+})
+
+
+
 
 
